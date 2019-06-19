e968bebe55c2f08dc6773469eeeb5c2a18787e93
ronaldalbertjr <ronaldalbert1609@gmail.com>
2019-06-19 01:45:07 -0300

    TermineiConjuntosDisjuntos

--- conjuntos.cpp
+++ conjuntos.cpp
@@ -5,13 +5,13 @@
 #include <ctype.h>
 using namespace std;
 
-int maior;
-
+int maior = 0;
 struct Elemento
 {
 	int valor;
 	int pai;
 	int rank;
+	int quantidadeDeElementos;
 };
 
 int find(struct Elemento subconjuntos[], int i)
@@ -27,15 +27,23 @@ void uniao(struct Elemento subconjuntos[], int x, int y)
 	int origemX = find(subconjuntos, x);
 	int origemY = find(subconjuntos, y);
 
+	if(origemX != origemY)
+	{
+		subconjuntos[origemX].quantidadeDeElementos += subconjuntos[origemY].quantidadeDeElementos;
+		subconjuntos[origemY].quantidadeDeElementos = subconjuntos[origemX].quantidadeDeElementos;
+	}
+
 	if(subconjuntos[origemX].rank < subconjuntos[origemY].rank)
 		subconjuntos[origemX].pai = origemY;
-	else if(subconjuntos[origemX].rank < subconjuntos[origemY].rank)
+	else if(subconjuntos[origemX].rank > subconjuntos[origemY].rank)
 		subconjuntos[origemY].pai = origemX;
 
 	else{
 		subconjuntos[origemY].pai = origemX;
 		subconjuntos[origemX].rank++;
 	}
+
+	maior = maior > subconjuntos[origemX].quantidadeDeElementos ? maior : subconjuntos[origemX].quantidadeDeElementos;
 }
 
 
@@ -43,14 +51,33 @@ int main()
 {
 	int P;
 	scanf("%d", &P);
+	int valor0, valor1;
 
 	struct Elemento subconjuntos[P];
-	for(int i = 0; i < P; i++)
+	struct Elemento* aux;
+	for(int i = 1; i <= P; i++)
 	{
-		subconjuntos[i] = (struct Elemento*) malloc( sizeof(struct Elemento) );
-		subconjuntos[i]->valor = i + 1;
-		subconjuntos[i]->pai = i + 1;
-		subconjuntos[i]->rank = 0;
+		aux = (struct Elemento*) malloc( sizeof(struct Elemento) );
+		aux->valor = i;
+		aux->pai = i;
+		aux->quantidadeDeElementos = 1;
+		aux->rank = 0;
+
+		subconjuntos[i] = *aux;
 	}
-	
+
+	while(!feof(stdin))
+	{
+		int d = scanf("%d", &valor0); 
+		if(d == 1)
+		{
+			scanf("%d", &valor1);
+			uniao(subconjuntos, valor0, valor1);
+		}
+		else
+		{
+			while ( (d = getchar()) != EOF && d != '\n' );
+			printf("|%d|\n", maior);
+		}
+	}
 }
